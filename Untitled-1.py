c266d86fc1fb45329b3f13bd26f2e2d9fabaa25a
Rundi <mostvk@gmail.com>
2021-08-06 08:28:56 +0300

    git in wsl

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,10 +1,7 @@
-from mysite.settings import PM_API_KEY
+from celery import Celery
 
-from postmarker.core import PostmarkClient
-postmark = PostmarkClient(server_token=PM_API_KEY)
-postmark.emails.send(
-    From='xayil89442@obxstorm.com',
-    To='xayil89442@obxstorm.com',
-    Subject='Postmark test',
-    HtmlBody='HTML body goes here'
-)
+app = Celery('tasks', broker='pyamqp://guest@localhost//')
+
+@app.task
+def add(x, y):
+    return x + y
